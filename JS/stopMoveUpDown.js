909c940bde29e141cda55b04b162cb7655cb8b7d
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2019-07-02 18:31:42 +0800

    Update stopMoveUpDown.js

--- JS/stopMoveUpDown.js
+++ JS/stopMoveUpDown.js
@@ -1,19 +1,13 @@
-
-scrollObj.targetY = null
 scrollObj.addEventListener('touchstart', function(e){
-  this.targetY = e.targetTouches[0].clientY
+  this._targetY = e.targetTouches[0].clientY
 })
 
 scrollObj.addEventListener('touchmove',function(e){
-  var NewTargetY = e.targetTouches[0].clientY,
-      iScrollTop = this.scrollTop,
-      iScrollHeight = this.scrollHeight,
-      iClientHeight = this.clientHeight;
-  
+  var NewTargetY = e.targetTouches[0].clientY
   // 向下
-  if(iScrollTop <= 0 && NewTargetY - this.targetY > 0){
+  if(this.scrollTop <= 0 && NewTargetY - this._targetY > 0){
     e.preventDefault()
-  } else if(iScrollTop >= iScrollHeight - iClientHeight && NewTargetY - this.targetY < 0){
+  } else if(this.scrollTop >= this.scrollHeight - this.clientHeight && NewTargetY - this._targetY < 0){
     e.preventDefault()
   }
   
