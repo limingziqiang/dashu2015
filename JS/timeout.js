516d34527b6a37b942bff82e246649ebb78a78a9
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2021-01-28 18:07:11 +0800

    Create timeout.js

---
+++ JS/timeout.js
@@ -0,0 +1,11 @@
+let timeout = function (delay) {
+     return new Promise((resolve, reject) => {   
+           setTimeout(() => {   
+                  try {
+                      resolve(1)
+                  } catch (e) {
+                      reject(0)
+                   }
+           }, delay);
+     })
+ }
