b695d85ac2ee770dd61896557f6504cb8b2801b0
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 09:58:52 +0800

    Create props父传子-子组件.vue

---
+++ vue/props父传子-子组件.vue
@@ -0,0 +1,19 @@
+<template>
+  <span>{{props.name}}</span>
+  // 可省略【props.】
+  <span>{{name}}</span>
+</template>
+
+<script setup>
+  // import { defineProps } from 'vue'
+  // defineProps在<script setup>中自动可用，无需导入
+  // 需在.eslintrc.js文件中【globals】下配置【defineProps: true】
+
+  // 声明props
+  const props = defineProps({
+    name: {
+      type: String,
+      default: ''
+    }
+  })  
+</script>
