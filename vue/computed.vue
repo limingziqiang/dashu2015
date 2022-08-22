1ab5271ac4b574df3f72d57271a98c3ec717836c
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 09:52:07 +0800

    Create computed.vue

---
+++ vue/computed.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import { computed, ref } from 'vue'
+
+  const count = ref(1)
+
+  // 通过computed获得doubleCount
+  const doubleCount = computed(() => {
+    return count.value * 2
+  })
+  // 获取
+  console.log(doubleCount.value)
+</script>
