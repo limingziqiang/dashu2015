28ef4f73b620e7be34cc8049e3bdf1146be0f6f8
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 10:08:10 +0800

    Create emit子传父-父组件.vue

---
+++ vue/emit子传父-父组件.vue
@@ -0,0 +1,18 @@
+<template>
+  <child :name='state.name' @updateName='updateName'/>  
+</template>
+
+<script setup>
+  import { reactive } from 'vue'
+  // 引入子组件
+  import child from './child.vue'
+
+  const state = reactive({
+    name: 'Jerry'
+  })
+  
+  // 接收子组件触发的方法
+  const updateName = (name) => {
+    state.name = name
+  }
+</script>
