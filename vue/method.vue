4257da806bcc4e694e61ba0b3f2b48789a33aa30
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 09:51:38 +0800

    Create method.vue

---
+++ vue/method.vue
@@ -0,0 +1,17 @@
+<template>
+  // 调用方法
+  <button @click='changeName'>按钮</button>  
+</template>
+
+<script setup>
+  import { reactive } from 'vue'
+
+  const state = reactive({
+    name: 'Jery'
+  })
+
+  // 声明method方法
+  const changeName = () => {
+    state.name = 'Tom'
+  }  
+</script>
