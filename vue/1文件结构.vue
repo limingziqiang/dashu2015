7725d4d1366b642c842ab0b1f2afa7b526682602
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 09:41:53 +0800

    Create 1文件结构.vue

---
+++ vue/1文件结构.vue
@@ -0,0 +1,12 @@
+// Vue2中，<template> 标签中只能有一个根元素，在Vue3中没有此限制
+<template>
+  // ...
+</template>
+
+<script setup>
+  // ...
+</script>
+
+<style lang="scss" scoped>
+  // 支持CSS变量注入v-bind(color)
+</style>
