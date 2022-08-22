ba0c98d571a38a3837ee26cff0eec32e2213492d
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 11:51:33 +0800

    Create ref子组件实例和defineExpose--父组件-获取一个子组件实例.vue

---
+++ vue/ref子组件实例和defineExpose--父组件-获取一个子组件实例.vue
@@ -0,0 +1,20 @@
+<template>
+  <child ref='childRef'/>
+</template>
+
+<script setup>
+  import { ref, nextTick } from 'vue'
+  // 引入子组件
+  import child from './child.vue'
+
+  // 子组件ref（TypeScript语法）
+  const childRef = ref<InstanceType<typeof child>>()
+  
+  // nextTick
+  nextTick(() => {
+    // 获取子组件name
+    console.log(childRef.value.name)
+    // 执行子组件方法
+    childRef.value.changeName()
+  })
+</script>
