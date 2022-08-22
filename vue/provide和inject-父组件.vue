ff7ca2217362707dc7187eb915c2e697520691cd
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 15:37:29 +0800

    Create provide和inject-父组件.vue

---
+++ vue/provide和inject-父组件.vue
@@ -0,0 +1,26 @@
+<template>
+  <child/>
+</template>
+
+<script setup>
+  import { ref, watch, provide } from 'vue'
+  // 引入子组件
+  import child from './child.vue'
+
+  let name = ref('Jerry')
+  // 声明provide
+  provide('provideState', {
+    name,
+    changeName: () => {
+      name.value = 'Tom'
+    }
+  })
+
+  // 监听name改变
+  watch(name, () => {
+    console.log(`name变成了${name}`)
+    setTimeout(() => {
+      console.log(name.value) // Tom
+    }, 1000)
+  })
+</script>
