2fc789411159e54601151943452442276261017e
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 14:50:30 +0800

    Create 路由useRoute和useRouter.vue

---
+++ vue/路由useRoute和useRouter.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import { useRoute, useRouter } from 'vue-router'
+	
+  // 必须先声明调用
+  const route = useRoute()
+  const router = useRouter()
+	
+  // 路由信息
+  console.log(route.query)
+
+  // 路由跳转
+  router.push('/newPage')
+</script>
