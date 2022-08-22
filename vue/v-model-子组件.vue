58eb5603f4d6ea2be5326ac5c682868e1e565a08
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 10:16:00 +0800

    Create v-model-子组件.vue

---
+++ vue/v-model-子组件.vue
@@ -0,0 +1,27 @@
+/*
+支持绑定多个v-model，v-model 是 v-model:modelValue 的简写
+绑定其他字段，如：v-model:name
+*/
+
+<template>
+  <span @click="changeInfo">我叫{{ modelValue }}，今年{{ age }}岁</span>
+</template>
+
+<script setup>
+  // import { defineEmits, defineProps } from 'vue'
+  // defineEmits和defineProps在<script setup>中自动可用，无需导入
+  // 需在.eslintrc.js文件中【globals】下配置【defineEmits: true】、【defineProps: true】
+
+  defineProps({
+    modelValue: String,
+    age: Number
+  })
+
+  const emit = defineEmits(['update:modelValue', 'update:age'])
+  const changeInfo = () => {
+    // 触发父组件值更新
+    emit('update:modelValue', 'Tom')
+    emit('update:age', 30)
+  }
+</script>
+
