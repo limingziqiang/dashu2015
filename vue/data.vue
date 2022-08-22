263a353f142f97c99aaff1c0923a615dd00cc8e3
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 09:43:55 +0800

    Create data.vue

---
+++ vue/data.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import { reactive, ref, toRefs } from 'vue'
+
+  // ref声明响应式数据，用于声明基本数据类型
+  const name = ref('Jerry')
+  // 修改
+  name.value = 'Tom'
+
+  // reactive声明响应式数据，用于声明引用数据类型
+  const state = reactive({
+    name: 'Jerry',
+    sex: '男'
+  })
+  // 修改
+  state.name = 'Tom'
+  
+  // 使用toRefs解构
+  const {name, sex} = toRefs(state)
+  // template可直接使用{{name}}、{{sex}}
+</script>
