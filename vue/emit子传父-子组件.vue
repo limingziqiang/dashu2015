acfc7e5043575a1d97a9f8f6572d751b9e384ea1
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 10:07:33 +0800

    Create emit子传父-子组件.vue

---
+++ vue/emit子传父-子组件.vue
@@ -0,0 +1,27 @@
+<template>
+  <span>{{props.name}}</span>
+  // 可省略【props.】
+  <span>{{name}}</span>
+  <button @click='changeName'>更名</button>
+</template>
+
+<script setup>
+  // import { defineEmits, defineProps } from 'vue'
+  // defineEmits和defineProps在<script setup>中自动可用，无需导入
+  // 需在.eslintrc.js文件中【globals】下配置【defineEmits: true】、【defineProps: true】
+	
+  // 声明props
+  const props = defineProps({
+    name: {
+      type: String,
+      default: ''
+    }
+  }) 
+  // 声明事件
+  const emit = defineEmits(['updateName'])
+  
+  const changeName = () => {
+    // 执行
+    emit('updateName', 'Tom')
+  }
+</script>
