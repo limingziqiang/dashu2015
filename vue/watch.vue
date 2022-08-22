f6ff34d125e91df035520e11bb4fcfd2d78d5226
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 09:56:51 +0800

    Create watch.vue

---
+++ vue/watch.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { watch, reactive } from 'vue'
+
+  const state = reactive({
+    count: 1
+  })
+
+  // 声明方法
+  const changeCount = () => {
+    state.count = state.count * 2
+  }
+
+  // 监听count
+  watch(
+    () => state.count,
+    (newVal, oldVal) => {
+      console.log(state.count)
+      console.log(`watch监听变化前的数据：${oldVal}`)
+      console.log(`watch监听变化后的数据：${newVal}`)
+    },
+    {
+      immediate: true, // 立即执行
+      deep: true // 深度监听
+    }
+  )
+</script>
