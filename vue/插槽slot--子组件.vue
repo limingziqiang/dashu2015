0f48bec12331563b1bd1cf17a907311d1a4d02f8
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2022-08-22 13:51:04 +0800

    Create 插槽slot--子组件.vue

---
+++ vue/插槽slot--子组件.vue
@@ -0,0 +1,24 @@
+<template>
+  // 匿名插槽
+  <slot/>
+  // 具名插槽
+  <slot name='title'/>
+  // 作用域插槽
+  <slot name="footer" :scope="state" />
+</template>
+
+<script setup>
+  import { useSlots, reactive } from 'vue'
+  const state = reactive({
+    name: '张三',
+    age: '25岁'
+  })
+  
+  const slots = useSlots()
+  // 匿名插槽使用情况
+  const defaultSlot = reactive(slots.default && slots.default().length)
+  console.log(defaultSlot) // 1
+  // 具名插槽使用情况
+  const titleSlot = reactive(slots.title && slots.title().length)
+  console.log(titleSlot) // 3
+</script>
