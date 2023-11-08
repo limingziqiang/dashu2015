a1e73d54e8fd63a61981fdaf2e039e2e4b62c0af
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2023-11-08 19:19:55 +0800

    Update 繁琐20231108.js

--- 繁琐20231108.js
+++ 繁琐20231108.js
@@ -3,3 +3,32 @@
 使用更高效的数据结构：在处理图片列表时，将数组this.picture改为使用Set数据结构，这样可以避免重复图片的添加。
 减少不必要的循环和条件判断：在图文类型的处理中，可以通过使用find方法来减少循环次数，并直接跳出循环。
 避免重复的属性访问：在图文类型的处理中，可以将res.content.segments存储在一个变量中，以避免重复访问该属性。
+
+const PostType = {
+  GRAPHIC: {
+    type: 1,
+    logMessage: '图文',
+    content: {
+      type: 2,
+      property: 'content'
+    },
+    picture: {
+      type: null,
+      property: 'pictureOne',
+      description: '封面',
+    },
+  },
+  LIGHT: {
+    type: 2,
+    logMessage: '文',
+    content: {
+      type: 0,
+      property: 'segments',
+    },
+    picture: {
+      type: 1,
+      property: 'picture',
+      description: '图片列表',
+    },
+  },
+};
